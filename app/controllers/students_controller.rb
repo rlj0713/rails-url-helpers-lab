1a09082f2871f983bb379d8e1bbd81f264bb59c5
Reid <rlj0713@gmail.com>
2021-05-17 20:04:07 -0400

    commit tree 1d677f8d665803df79ca6e179fd6790aa7e539a3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,9 +8,20 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student = @student.toggle :active
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
 end
+
+
+# _url  http://www.example.com/students
+# _path /students
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create( first_name: "John", last_name: "Smith", active: true )
+Student.create( first_name: "Poca", last_name: "hontas" )
